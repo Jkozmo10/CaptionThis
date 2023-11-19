eeb73d3b3d25a8cf79b3c532ae4aa965fb90fb1f
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-19 09:35:53 -0800

    Improved threading with a P/C Model

--- TrainingSets/threaded_download_images.py
+++ TrainingSets/threaded_download_images.py
@@ -1,45 +1,63 @@
 import os
 import requests
 import csv
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Constants
-output_directory = "Images"
-os.makedirs(output_directory, exist_ok=True)
+OUTPUT_DIRECTORY = "Images"
+os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
 
-tsv_files = [
-    "Image_Labels_Subset_Train_GCC-Labels-training.tsv",
-    "Train_GCC-training.tsv",
-    "Validation_GCC-1.1.0-Validation.tsv"
-]
-
+TSV_FILE = "Train_GCC-training.tsv"
 SUCCESS_STATUS_CODE = 200
 TIMEOUT_TIME = 5
-NUMBER_OF_THREADS = 8
+NUMBER_OF_THREADS = 10
+MAXIMUM_NUMBER_OF_IMAGES = 100000
 
 def download_image(idx, caption, image_url):
     try:
         response = requests.get(image_url, timeout=TIMEOUT_TIME)
         if response.status_code == SUCCESS_STATUS_CODE:
-            image_filename = f"{idx + 1}.jpg"
-            image_path = os.path.join(output_directory, image_filename)
+            image_filename = f"{idx}.jpg"
+            image_path = os.path.join(OUTPUT_DIRECTORY, image_filename)
             with open(image_path, "wb") as image_file:
                 image_file.write(response.content)
-            print(f"Downloaded: {image_filename}")
-        else:
-            print(f"Failed to download: {image_url}")
+            logging.info(f"Downloaded: {image_filename}")
+            return image_filename, caption, image_url
     except Exception as e:
-        print(f"Error downloading image {idx + 1}: {str(e)}")
+        logging.error(f"Error downloading image {idx}: {str(e)}")
+    return None
+
+def process_images():
+    with ThreadPoolExecutor(max_workers=NUMBER_OF_THREADS) as executor:
+        future_to_image = {}
+        with open(TSV_FILE, "r", encoding="utf-8") as file:
+            tsv_reader = csv.reader(file, delimiter="\t")
+            for idx, (caption, image_url) in enumerate(tsv_reader):
+                if idx >= MAXIMUM_NUMBER_OF_IMAGES:
+                    break
+                future = executor.submit(download_image, idx, caption, image_url)
+                future_to_image[future] = idx
+
+        downloaded_images_info = []
+        for future in as_completed(future_to_image):
+            result = future.result()
+            if result:
+                downloaded_images_info.append(result)
+
+    return downloaded_images_info
 
 if __name__ == "__main__":
-    with open(tsv_files[1], "r", encoding="utf-8") as file:
-        tsv_reader = csv.reader(file, delimiter="\t")
-        tasks = []
-        for idx, (caption, image_url) in enumerate(tsv_reader):
-            tasks.append((idx, caption, image_url))
+    downloaded_images_info = process_images()
 
-        with ThreadPoolExecutor(max_workers=NUMBER_OF_THREADS) as executor:
-            for task in tasks:
-                executor.submit(download_image, *task)
+    downloaded_images_csv = os.path.join(OUTPUT_DIRECTORY, "downloaded_images.csv")
+    with open(downloaded_images_csv, "w", encoding="utf-8") as file:
+        csv_writer = csv.writer(file, delimiter=",")
+        csv_writer.writerow(["Image Filename", "Caption", "Image URL"])
+        for info in downloaded_images_info:
+            csv_writer.writerow(info)
 
-    print("Download complete.")
+    logging.info("Download complete.")
