8299632a7a173761dbb48341403a348d0e21d85f
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-27 13:45:44 -0800

    Updated Models

--- training/image_caption_dataset.py
+++ training/image_caption_dataset.py
@@ -1,36 +1,31 @@
-# image_caption_dataset.py
-
 import csv
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
+import logging
 
 class ImageCaptionDataset(Dataset):
     def __init__(self, csv_file, transform=None):
         self.data = []
-        with open(csv_file, 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                self.data.append(row)
         self.transform = transform
+        try:
+            with open(csv_file, 'r') as file:
+                reader = csv.reader(file)
+                self.data = [row for row in reader]
+        except IOError as e:
+            logging.error(f"Error reading CSV file: {e}")
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
+        image_path, caption, _ = self.data[idx]
         try:
-            image_path, caption, _ = self.data[idx]
-            image_path = "/Users/nero/Desktop/CaptionThis/data_sets/Images/" + image_path
             image = Image.open(image_path).convert("RGB")
             if self.transform:
                 image = self.transform(image)
             return image, caption
-        except UnidentifiedImageError:
+        except (IOError, UnidentifiedImageError) as e:
+            logging.error(f"Error loading image: {e}")
             return None, None
 
-# Example transformation, can be customized as needed
-def get_transform():
-    return transforms.Compose([
-        transforms.Resize((300, 300)),
-        transforms.ToTensor(),
-    ])
--- training/train_blip_model.py
+++ training/train_blip_model.py
@@ -1,49 +1,71 @@
-# train_blip_model.py
-
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from image_caption_dataset import ImageCaptionDataset, get_transform
+from tqdm import tqdm
+import logging
+
+def get_transform():
+    return transforms.Compose([
+        transforms.Resize((300, 300)),
+        transforms.ToTensor(),
+    ])
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
-    # Set up dataset and dataloader
-    csv_file_path = '/Users/nero/Desktop/CaptionThis/data_sets/Images/sorted_output.csv'
-    transform = get_transform()
-    dataset = ImageCaptionDataset(csv_file_path, transform=transform)
-    train_dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    try:
+        # Dataset setup
+        csv_file_path = '/path/to/sorted_output.csv'
+        transform = get_transform()
+        dataset = ImageCaptionDataset(csv_file_path, transform=transform)
+        train_size = int(0.8 * len(dataset))
+        val_size = len(dataset) - train_size
+        train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-    # Initialize the BLIP model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_name = "Salesforce/blip-image-captioning-base"
-    processor = BlipProcessor.from_pretrained(model_name)
-    model = BlipForConditionalGeneration.from_pretrained(model_name)
-    model.to(device)
+        train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+        val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
-    # Training setup
-    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
-    model.train()
-    num_epochs = 50  # Define the number of epochs
+        # Model and Training Setup
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+        model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+        model.to(device)
 
-    # Training loop
-    for epoch in range(num_epochs):
-        print(f"Epoch: {epoch}")
-        for idx, (images, captions) in enumerate(train_dataloader):
-            images = images.to(device)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+        num_epochs = 50
 
-            # Process images and captions for BLIP model
-            inputs = processor(images=images, text=captions, return_tensors="pt", padding=True, truncation=True).to(device)
-            outputs = model(**inputs)
+        # Training Loop
+        model.train()
+        for epoch in range(num_epochs):
+            logging.info(f"Epoch: {epoch+1}/{num_epochs}")
+            for images, captions in tqdm(train_dataloader):
+                try:
+                    images, captions = images.to(device), captions.to(device)
+                    inputs = processor(images=images, text=captions, return_tensors="pt", padding=True, truncation=True).to(device)
+                    outputs = model(**inputs)
 
-            loss = outputs.loss
-            if loss is not None:
-                print(f"Loss: {loss.item()}")
-                loss.backward()
+                    optimizer.zero_grad()
+                    if outputs.loss is not None:
+                        outputs.loss.backward()
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                        optimizer.step()
 
-            optimizer.zero_grad()
-            optimizer.step()
+                except Exception as e:
+                    logging.error(f"Error during training: {e}")
 
-    # Save the trained model
-    torch.save(model.state_dict(), 'trained_blip_model.pth')
+            scheduler.step()
+
+            # Save Checkpoint
+            torch.save(model.state_dict(), f'trained_blip_model_epoch_{epoch+1}.pth')
+
+        logging.info("Training complete.")
+
+    except Exception as e:
+        logging.error(f"Failed to complete training: {e}")
 
 if __name__ == "__main__":
     main()
+
