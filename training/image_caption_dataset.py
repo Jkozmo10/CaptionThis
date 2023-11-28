7782c3d1b8dd150440b9827f9de44f7c7adc76db
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-27 17:13:21 -0800

    Simplified dataset and blip model training code

--- training/image_caption_dataset.py
+++ training/image_caption_dataset.py
@@ -1,31 +1,36 @@
 import csv
 from PIL import Image
-from torch.utils.data import Dataset
 from torchvision import transforms
-import logging
+import torch
+from torch.utils.data import Dataset, DataLoader
+import os
 
-class ImageCaptionDataset(Dataset):
-    def __init__(self, csv_file, transform=None):
+class ImageCaptioningDataset(Dataset):
+    def __init__(self, csv_file, transform, processor):
         self.data = []
         self.transform = transform
-        try:
-            with open(csv_file, 'r') as file:
-                reader = csv.reader(file)
-                self.data = [row for row in reader]
-        except IOError as e:
-            logging.error(f"Error reading CSV file: {e}")
+        self.processor = processor
+        self.image_dir = '/Users/nero/Desktop/CaptionThis/data_sets/Images'  # Update with the path to your image directory
+
+        # Read the CSV file and store the data
+        with open(csv_file, 'r', encoding='utf-8') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                self.data.append(row)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        image_path, caption, _ = self.data[idx]
-        try:
-            image = Image.open(image_path).convert("RGB")
-            if self.transform:
-                image = self.transform(image)
-            return image, caption
-        except (IOError, UnidentifiedImageError) as e:
-            logging.error(f"Error loading image: {e}")
-            return None, None
+        item = self.data[idx]
+        image_path = os.path.join(self.image_dir, item["Image Filename"])
+        image = Image.open(image_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+
+        # Use processor to process the image and caption
+        encoding = self.processor(images=image, text=item["Caption"], padding="max_length", return_tensors="pt")
+        encoding = {k: v.squeeze(0) for k, v in encoding.items()}
+
+        return encoding
 
--- training/train_blip_model.py
+++ training/train_blip_model.py
@@ -1,132 +1,68 @@
-# Import necessary libraries
-import os
-import requests
+from transformers import AutoProcessor, BlipForConditionalGeneration
+from torch.utils.data import DataLoader
+
 import csv
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import logging
-from PIL import Image, UnidentifiedImageError
-from io import BytesIO
-import torch
-from torch.utils.data import DataLoader, random_split
+from PIL import Image
 from torchvision import transforms
-from transformers import BlipProcessor, BlipForConditionalGeneration
-from tqdm import tqdm
+import torch
+from torch.utils.data import Dataset, DataLoader
+from image_caption_dataset import ImageCaptioningDataset
 
-# Setup logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Constants
-OUTPUT_DIRECTORY = "Images"
-os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
-TSV_FILE = "Train_GCC-training.tsv"
-SUCCESS_STATUS_CODE = 200
-TIMEOUT_TIME = 5
-NUMBER_OF_THREADS = 10
-MAXIMUM_NUMBER_OF_IMAGES = 35000
-OUTPUT_CSV_DIR = "."
-OUTPUT_CSV = "downloaded_images.csv"
+# Initialize processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-class ImageCaptionDataset(torch.utils.data.Dataset):
-    def __init__(self, csv_file, transform=None, processor=None):
-        self.data = []
-        self.transform = transform
-        self.processor = processor  # Add the processor as an attribute
-        self.image_dir = '/Users/nero/Desktop/CaptionThis/data_sets/Images'
-        try:
-            with open(csv_file, 'r') as file:
-                reader = csv.reader(file)
-                next(reader)  # Skip the header row
-                self.data = [row for row in reader]
-        except IOError as e:
-            logging.error(f"Error reading CSV file: {e}")
+# Assuming 'dataset' is already defined or loaded
+# Create an instance of your custom dataset
+csv_file_path = "/Users/nero/Desktop/CaptionThis/data_sets/downloaded_images.csv"
+transform = transforms.Compose([
+    transforms.Resize((300, 300)),
+    transforms.ToTensor(),
+])
+train_dataset = ImageCaptioningDataset(csv_file_path, transform, processor)
 
-    def __len__(self):
-        return len(self.data)
+# Create a DataLoader for the training dataset
+train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=2)
 
-    def __getitem__(self, idx):
-        image_filename, caption = self.data[idx][1], self.data[idx][2]
-        image_path = os.path.join(self.image_dir, image_filename)
-        try:
-            image = Image.open(image_path).convert("RGB")
-            if self.transform:
-                image = self.transform(image)
-            caption = self.data[idx][2]  # Caption as a string
-            return image, caption  # Return the caption as a string
-        except (IOError, UnidentifiedImageError) as e:
-            logging.error(f"Error loading image {image_path}: {e}")
-            return None, None
+# Training setup (optimizer, device setup)
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)  # Optional, if you want to use a scheduler
 
-# Define the transformation applied to each image
-def get_transform():
-    return transforms.Compose([
-        transforms.Resize((300, 300)),
-        transforms.ToTensor(),
-    ])
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
 
-# Define the main function for training
-def main():
-    try:
-        # Dataset setup
-        csv_file_path = '/Users/nero/Desktop/CaptionThis/data_sets/downloaded_images.csv'
-        transform = get_transform()
+# Training loop
+TOTAL_EPOCHS = 10
+for epoch in range(TOTAL_EPOCHS):
+    print("Epoch:", epoch)
+    model.train()
 
-        # Model and Training Setup
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-        dataset = ImageCaptionDataset(csv_file_path, transform=transform, processor=processor)
-        train_size = int(0.8 * len(dataset))
-        val_size = len(dataset) - train_size
-        train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    total_loss = 0.0
+    for idx, batch in enumerate(train_dataloader):
+        optimizer.zero_grad()  # Reset gradients
 
-        train_dataloader = DataLoader(train_dataset, batch_size=3, shuffle=True)
-        val_dataloader = DataLoader(val_dataset, batch_size=3, shuffle=False)
+        input_ids = batch["input_ids"].to(device)
+        pixel_values = batch["pixel_values"].to(device)
 
-        model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
-        model.to(device)
+        outputs = model(input_ids=input_ids, pixel_values=pixel_values, labels=input_ids)
+        loss = outputs.loss
 
-        optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
-        num_epochs = 10
+        if loss is not None:
+            loss.backward()
+            optimizer.step()
 
-        for epoch in range(num_epochs):
-            logging.info(f"Epoch {epoch+1}/{num_epochs}")
-            model.train()  # Ensure the model is in training mode
-            total_loss = 0.0
+            total_loss += loss.item()
+            print(f"Batch {idx} Loss: {loss.item()}")
+        else:
+            print(f"Batch {idx} Loss: None")
 
-            for batch_idx, (images, captions) in enumerate(train_dataloader):
-                try:
-                    images = images.to(device)
-                    inputs = processor(images=images, text=list(captions), return_tensors="pt", padding=True, truncation=True).to(device)
-                    outputs = model(**inputs)
 
-                    loss = outputs.loss
-                    if loss is not None:
-                        total_loss += loss.item()
-                        optimizer.zero_grad()
-                        loss.backward()
-                        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-                        optimizer.step()
-                        logging.info(f"Epoch {epoch+1}/{num_epochs}, Batch {batch_idx+1}/{len(train_dataloader)}, Loss: {loss.item()}")
-                    else:
-                        print("Loss is None")
+    scheduler.step()  # Adjust the learning rate
 
-                except AttributeError as e:
-                    logging.error(f"Attribute error in batch {batch_idx}: {e}")
-                    continue  # Skip this batch due to an error
-                except Exception as e:
-                    logging.error(f"Unexpected error during training at batch {batch_idx}: {e}")
-                    continue  # Skip this batch due to an error
+    # Save the model after each epoch (optional but recommended)
+    torch.save(model.state_dict(), f'model_epoch_{epoch}.pth')
 
-            avg_loss = total_loss / len(train_dataloader)
-            logging.info(f"Epoch {epoch+1}/{num_epochs} completed with average loss: {avg_loss}")
-            scheduler.step()  # Adjust the learning rate based on the scheduler
-            torch.save(model.state_dict(), f'trained_blip_model_epoch_{epoch+1}.pth')
-
-        logging.info("Training complete.")
-
-    except Exception as e:
-        logging.error(f"Failed to complete training: {e}")
-
-if __name__ == "__main__":
-    main()
-
+    # Print average loss
+    avg_loss = total_loss / len(train_dataloader)
+    print(f"Average Loss for Epoch {epoch}: {avg_loss}")
